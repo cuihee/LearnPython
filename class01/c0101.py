8d3f94bd773d5c70d7aa96aa3737864feac1f7bb
cuihe <c253406143@163.com>
2019-10-22 10:24:39 +0800

    基础

--- class01/c0101.py
+++ class01/c0101.py
@@ -5,4 +5,3 @@ print("菜鸟python3教程 https://www.runoob.com/python3/python3-tutorial.html 
 print("官方中文python3.8教程 https://docs.python.org/zh-cn/3.8/tutorial/index.html")
 print("PyTorch中文手册 https://github.com/zergtant/pytorch-handbook")
 print("PyTorch官方中文文档 https://pytorch-cn.readthedocs.io/zh/latest/")
-
---
+++ class01/c0102.py
@@ -0,0 +1,24 @@
+
+# 一行注释 注意缩进
+if True:
+    print("python的缩进很关键")
+else:
+    print \
+        (r"python的一条语句跨行时使用\反斜杠放在行尾来实现")
+
+
+"""
+多行注释
+方式一
+"""
+
+'''
+多行注释
+方式二
+'''
+
+import keyword
+print("\npython保留字 ", keyword.kwlist)
+
+print("\npython的数字类型有四个 \nint 1 2 3\nbool True False\nfloat 1.23 3E-2\ncomplex 1+2j 1.1+2.2j")
+
