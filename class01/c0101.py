eb41202d8d97c593d7ace3f95d739d6e3bfebb7d
cuihe <c253406143@163.com>
2019-10-22 10:02:27 +0800

    Initial commit

---
+++ class01/c0101.py
@@ -0,0 +1,8 @@
+
+print("环境配置可以参考 https://www.runoob.com/python3/python3-install.html")
+print("一些有用的网址")
+print("菜鸟python3教程 https://www.runoob.com/python3/python3-tutorial.html ")
+print("官方中文python3.8教程 https://docs.python.org/zh-cn/3.8/tutorial/index.html")
+print("PyTorch中文手册 https://github.com/zergtant/pytorch-handbook")
+print("PyTorch官方中文文档 https://pytorch-cn.readthedocs.io/zh/latest/")
+
