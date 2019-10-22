be2b322a7e91e12e774dd4bab11c29989d9cf5e7
cuihe <c253406143@163.com>
2019-10-22 10:58:49 +0800

    基础

--- class01/c0101.py
+++ class01/c0101.py
@@ -3,5 +3,7 @@ print("环境配置可以参考 https://www.runoob.com/python3/python3-install.h
 print("一些有用的网址")
 print("菜鸟python3教程 https://www.runoob.com/python3/python3-tutorial.html ")
 print("官方中文python3.8教程 https://docs.python.org/zh-cn/3.8/tutorial/index.html")
+print("菜鸟Numpy教程 https://www.runoob.com/numpy/numpy-tutorial.html")
+print("官方中文Numpy教程 https://www.numpy.org.cn/")
 print("PyTorch中文手册 https://github.com/zergtant/pytorch-handbook")
 print("PyTorch官方中文文档 https://pytorch-cn.readthedocs.io/zh/latest/")
