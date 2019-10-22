e50444c823d63320c58d0b7d63d280fcc6fef079
cuihe <c253406143@163.com>
2019-10-22 10:37:45 +0800

    基础

--- class01/c0102.py
+++ class01/c0102.py
@@ -10,6 +10,12 @@ else:
 """
 多行注释
 方式一
+
+注意安装GIT
+准备github账号密码
+pycharm中VCS-import into version control-share project on github 
+VCS中的commit和update是和本地git交互的
+VCS中Git-Pull Push是和远程github交互的
 """
 
 '''
