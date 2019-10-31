b1e4f239bf4ee8ddece1d62199f942a2bbcf49d0
cuihe <c253406143@163.com>
2019-10-31 20:51:02 +0800

    第二天学习 和可以提出的问题

---
+++ Day02/c0203.py
@@ -0,0 +1,7 @@
+"""
+python内置函数有哪些
+"""
+print('非官方', 'https://www.runoob.com/python/python-built-in-functions.html')
+
+
+print('官方 3.7.5', 'https://docs.python.org/zh-cn/3.7/library/functions.html')
